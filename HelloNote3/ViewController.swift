3ae98b258e3be954ad9fc2afbdfb33d2fe68f47a
Hidemaro Fujinami <guppy4.spurs21@gmail.com>
2020-06-16 23:25:22 +0900

    Initial Commit

---
+++ HelloNote3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloNote3
+//
+//  Created by Hidemaro Fujinami on 2020/06/16.
+//  Copyright © 2020 maro525. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
