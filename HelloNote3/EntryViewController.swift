c12a41499c5c63484e3187f1519c08e27b47816b
Hidemaro Fujinami <guppy4.spurs21@gmail.com>
2020-06-17 22:42:02 +0900

    first commit

---
+++ HelloNote3/EntryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryViewController.swift
+//  HelloNote3
+//
+//  Created by Hidemaro Fujinami on 2020/06/16.
+//  Copyright © 2020 maro525. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
